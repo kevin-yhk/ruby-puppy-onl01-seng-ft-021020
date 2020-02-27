95a859dd4d83d29d51eb01b0f84267a6d7716b2c
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-27 00:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    save 
   end 
   
   def self.all 
@@ -20,5 +20,9 @@ class Dog
     @@all.select.each do |dog|
       puts dog.name 
     end 
+  end 
+  
+  def save
+    @@all << self 
   end 
 end 
