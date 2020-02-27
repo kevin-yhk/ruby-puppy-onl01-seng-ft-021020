6580636f43e7cd383bdc9699668e0afe699088cc
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-27 00:50:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,6 +6,7 @@ class Dog
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     ##@@all << self
     @save = save
   end
@@ -19,14 +20,25 @@ class Dog
       puts dog.name
     end
   end
+=======
+    @@all << self
+  end
+
+  def self.all
+    @all
+    end
+>>>>>>> b3fd9e3b12a6de3cd7bf2d1c79adf83a2c84a016
 
   def self.clear_all
     @@all.clear
   end
+<<<<<<< HEAD
   
   def save
     self.class.all << self
   end 
     
+=======
+>>>>>>> b3fd9e3b12a6de3cd7bf2d1c79adf83a2c84a016
 
 end
